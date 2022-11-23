4e1a37ff6a7d7392fdd045a5b1636c2888e25fcc
giuseppe <snesnopic@gmail.com>
2022-11-23 12:21:58 +0100

    Bozza di funzionalità del pulsante di login

--- app/src/main/java/com/snesnopic/ermes/MainActivity.java
+++ app/src/main/java/com/snesnopic/ermes/MainActivity.java
@@ -1,14 +1,35 @@
 package com.snesnopic.ermes;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button loginButton;
+    String email;
+    String password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(view -> {
+            EditText em = findViewById(R.id.editTextTextEmailAddress);
+            EditText pw = findViewById(R.id.editTextTextPassword);
+            email = em.getText().toString();
+            password = pw.getText().toString();
+            if(email.isEmpty() || password.isEmpty())
+            {
+                new AlertDialog.Builder(this)
+                        .setMessage(R.string.fieldsError)
+                        // A null listener allows the button to dismiss the dialog and take no further action.
+                        .setNegativeButton(android.R.string.ok, null)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+            }
+
+        });
     }
 }
