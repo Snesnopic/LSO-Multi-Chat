093c71c1ed99025c5dafaa6a2e559a34bc93b429
giuseppe <snesnopic@gmail.com>
2022-11-23 14:40:08 +0100

    Bozza di MainActivity (aggiunto solo listener del pulsante new group)

--- app/src/main/java/com/snesnopic/ermes/MainActivity.java
+++ app/src/main/java/com/snesnopic/ermes/MainActivity.java
@@ -1,35 +1,20 @@
 package com.snesnopic.ermes;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.app.AlertDialog;
+import android.content.Context;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
-    Button loginButton;
-    String email;
-    String password;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        loginButton = findViewById(R.id.loginButton);
-        loginButton.setOnClickListener(view -> {
-            EditText em = findViewById(R.id.editTextTextEmailAddress);
-            EditText pw = findViewById(R.id.editTextTextPassword);
-            email = em.getText().toString();
-            password = pw.getText().toString();
-            if(email.isEmpty() || password.isEmpty())
-            {
-                new AlertDialog.Builder(this)
-                        .setMessage(R.string.fieldsError)
-                        // A null listener allows the button to dismiss the dialog and take no further action.
-                        .setNegativeButton(android.R.string.ok, null)
-                        .setIcon(android.R.drawable.ic_dialog_alert)
-                        .show();
-            }
-
+    FloatingActionButton fab;
+    MainActivity(Bundle savedInstanceState, Context context)
+    {
+        fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(view -> {
+            setContentView(R.layout.create_group_activity);
         });
     }
 }
+
