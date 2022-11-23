a8607089cff013dcf90129785a40662a3b43832e
giuseppe <snesnopic@gmail.com>
2022-11-23 16:01:23 +0100

    Activity del crea gruppo

---
+++ app/src/main/java/com/snesnopic/ermes/CreateGroupActivity.java
@@ -0,0 +1,13 @@
+package com.snesnopic.ermes;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateGroupActivity extends AppCompatActivity {
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.create_group_activity);
+
+    }
+}
