a8323aa7252df8fc3fd687e9e75b5973725c4eb4
giuseppe <snesnopic@gmail.com>
2022-11-25 15:38:10 +0100

    Metodo provaLogin "finalizzato" e risolto un errore nel singleton (non lo era)

--- app/src/main/java/com/snesnopic/ermes/Connessione.java
+++ app/src/main/java/com/snesnopic/ermes/Connessione.java
@@ -1,22 +1,22 @@
 package com.snesnopic.ermes;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Connessione extends Thread {
+    static PrintWriter pw;
     static String statichostname;
     static int staticport;
-    Socket socket;
+    Socket s;
     private static Connessione instance;
     public static Connessione getInstance(String hostname, int port){
         if(instance == null) {
             statichostname = hostname;
             staticport = port;
-            return new Connessione();
-
+            instance = new Connessione();
         }
-        else
-            return instance;
+        return instance;
     }
 
     private Connessione() {
@@ -24,9 +24,49 @@ public class Connessione extends Thread {
     }
     public void run() {
         try {
-            socket = new Socket(statichostname, staticport);
+            s = new Socket(statichostname, staticport);
+            pw = new PrintWriter(s.getOutputStream(), true);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+    boolean provaLogin(String email, String password, boolean register)
+    {
+        try {
+            pw.println(email);
+            pw.println(password);
+            //ritorna vero se riceve 1 (login success) altrimenti 0
+            int response = s.getInputStream().read();
+            switch(response) {
+                default:
+                case 0:
+                    return false;
+                case 1:
+                    if(register) {
+                        s.getOutputStream().write(1);
+                        return false;
+                    }
+                    else
+                    {
+                        s.getOutputStream().write(0);
+                        return true;
+                    }
+                case 2:
+                    if(register) {
+                        s.getOutputStream().write(1);
+                        return true;
+                    }
+                    else
+                    {
+                        s.getOutputStream().write(0);
+                        return false;
+                    }
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
         }
     }
 }
