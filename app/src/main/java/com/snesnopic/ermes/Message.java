c380dcda51177c97dfe2ff43c8f6c1010ebbf5ed
giuseppe <snesnopic@gmail.com>
2022-11-25 20:26:11 +0100

    Classe di dati per i messaggi

---
+++ app/src/main/java/com/snesnopic/ermes/Message.java
@@ -0,0 +1,9 @@
+package com.snesnopic.ermes;
+
+import java.time.LocalDateTime;
+
+public class Message {
+    public String message;
+    public LocalDateTime time;
+    public String senderUsername;
+}
