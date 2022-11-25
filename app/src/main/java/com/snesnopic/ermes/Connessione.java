fe04f93e54bddff8940d1e3ddd161797fd5ccd69
Gheovgos <giorgio.longobardo2@gmail.com>
2022-11-25 12:11:25 +0100

    prima implementazione dei socket

---
+++ app/src/main/java/com/snesnopic/ermes/Connessione.java
@@ -0,0 +1,34 @@
+package com.snesnopic.ermes;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class Connessione extends Thread {
+    static String statichostname;
+    static int staticport;
+    Socket socket;
+    private static Connessione instance;
+    public static Connessione getInstance(String hostname, int port){
+        if(instance == null) {
+            statichostname = hostname;
+            staticport = port;
+            Connessione c = new Connessione();
+            c.start();
+            return c;
+
+        }
+        else
+            return instance;
+    }
+
+    private Connessione() {
+
+    }
+    public void run() {
+        try {
+            socket = new Socket(statichostname, staticport);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
