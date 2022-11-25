01ff96646dd87fffd3c2b95b8c9103cdc077027a
giuseppe <snesnopic@gmail.com>
2022-11-25 11:05:28 +0100

    Il metodo onViewCreated verrà usato per aggiungere dinamicamente elementi

--- app/src/main/java/com/snesnopic/ermes/MyGroupsFragment.java
+++ app/src/main/java/com/snesnopic/ermes/MyGroupsFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class MyGroupsFragment extends Fragment {
@@ -13,5 +16,11 @@ public class MyGroupsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_mygroups, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ScrollView sv = view.findViewById(R.id.mygroupsScrollView);
+
     }
 }
