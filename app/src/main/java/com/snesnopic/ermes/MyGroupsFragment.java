af0b51ddf9d34e043006311c85c13d1acf955ac0
giuseppe <snesnopic@gmail.com>
2022-11-25 12:10:15 +0100

    Bozza di lista di gruppi in mygroups

--- app/src/main/java/com/snesnopic/ermes/MyGroupsFragment.java
+++ app/src/main/java/com/snesnopic/ermes/MyGroupsFragment.java
@@ -4,13 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ScrollView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class MyGroupsFragment extends Fragment {
+import java.util.List;
 
+
+public class MyGroupsFragment extends Fragment {
+    public List<Group> GetGroups(int userID)
+    {
+        return null;
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -20,7 +28,15 @@ public class MyGroupsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        ScrollView sv = view.findViewById(R.id.mygroupsScrollView);
 
+        ListView list = view.findViewById(R.id.listView1);
+        String s1 = "1";
+        String s2 = "2";
+        String array[] = {"1","2"};
+
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(view.getContext(), R.layout.fragment_mygroups,R.id.textView,array);
+        list.setAdapter(adapter);
     }
 }
