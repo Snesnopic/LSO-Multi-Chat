63ae3b1e0ac0bf53aafa23e6881b4906cfed7397
Giuseppe <snesnopic@gmail.com>
2022-12-04 14:54:59 +0100

    ActionBar rimossa in XML invece che programmaticamente

--- app/src/main/java/com/snesnopic/ermes/MainActivity.java
+++ app/src/main/java/com/snesnopic/ermes/MainActivity.java
@@ -26,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();     //nasconde il nome sopra l'app
         setContentView(R.layout.activity_main);
         tabLayout = findViewById(R.id.tabLayout);
         viewPager = findViewById(R.id.viewPager);
