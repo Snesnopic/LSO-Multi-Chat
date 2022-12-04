700841e5b9fea769cc5e6814aee81fa40e51057d
Giuseppe <snesnopic@gmail.com>
2022-12-04 16:38:34 +0100

    Fragment java del tab delle richieste

--- app/src/main/java/com/snesnopic/ermes/RequestsFragment.java
+++ app/src/main/java/com/snesnopic/ermes/RequestsFragment.java
@@ -4,14 +4,39 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import com.snesnopic.ermes.datapkg.Group;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RequestsFragment extends Fragment {
+    public List<Group> GetGroupsWithRequests()
+    {
+        ArrayList<Group> groupsWithRequests = new ArrayList<>();
+        for(int i = 0; i < 5; ++i)
+        {
+            Group a = new Group();
+            a.name = "Gruppo " + i;
+            groupsWithRequests.add(a);
+        }
+        return groupsWithRequests;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_requests, container, false);
+        return inflater.inflate(R.layout.fragment_groupswithrequests, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ListView list = view.findViewById(R.id.groupsWithRequestListView);
+        GroupsWithRequestsAdapter adapter = new GroupsWithRequestsAdapter(view.getContext(),GetGroupsWithRequests());
+        list.setAdapter(adapter);
     }
 }
