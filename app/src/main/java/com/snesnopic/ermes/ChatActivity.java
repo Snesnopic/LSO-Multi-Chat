32245451aceeba157ea470deefdbd24f6852b53d
giuseppe <snesnopic@gmail.com>
2022-12-06 16:56:41 +0100

    Aggiunto ChatActivity, anche al manifest

--- app/src/main/java/com/snesnopic/ermes/ChatActivity.java
+++ app/src/main/java/com/snesnopic/ermes/ChatActivity.java
@@ -1,6 +1,36 @@
 package com.snesnopic.ermes;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.snesnopic.ermes.datapkg.Message;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class ChatActivity extends AppCompatActivity {
+    ArrayList<Message> GetMessages()
+    {
+        ArrayList<Message> messages = new ArrayList<>();
+        for(int i = 0; i < 5; ++i)
+        {
+            Message msg = new Message();
+            msg.senderUsername = "Utente "+i;
+            msg.message = "PROVA " + i;
+            msg.time = LocalDateTime.now();
+            messages.add(msg);
+        }
+        return messages;
+    }
+    RecyclerView list;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chat_activity);
+        list = findViewById(R.id.recycler_gchat);
+        ArrayList<Message> messages = GetMessages();
+        MessageAdapter adapter = new MessageAdapter(this, messages.size(),messages);
+        list.setLayoutManager(new LinearLayoutManager(this));
+        list.setAdapter(adapter);
+    }
 }
