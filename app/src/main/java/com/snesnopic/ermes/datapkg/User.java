3be9acea04d49ee21dc10d223a49b2ae17bbfb28
Giuseppe <snesnopic@gmail.com>
2023-01-22 12:12:52 +0100

    Costruttori utili al debugging

--- app/src/main/java/com/snesnopic/ermes/datapkg/User.java
+++ app/src/main/java/com/snesnopic/ermes/datapkg/User.java
@@ -4,5 +4,12 @@ public class User {
     public String username;
     public String password;
     public int userid;
+    public User()
+    {
 
+    }
+    public User(String username)
+    {
+        this.username = username;
+    }
 }
