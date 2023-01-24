605b05b1fc0bf688aae9588fba1b30f229667399
Giuseppe <snesnopic@gmail.com>
2023-01-24 11:24:37 +0100

    Create Group non è più una Activity, ora contiene solo l'EditText

---
+++ app/src/main/java/com/snesnopic/ermes/activitypkg/CreateGroupDialog.java
@@ -0,0 +1,24 @@
+package com.snesnopic.ermes.activitypkg;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import androidx.fragment.app.DialogFragment;
+import com.snesnopic.ermes.R;
+
+public class CreateGroupDialog extends DialogFragment {
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.create_group_dialog, null));
+        builder.setMessage(R.string.insert_group_name)
+                .setPositiveButton(R.string.create, (dialog, id) -> {
+                    //TODO: crea gruppo, check del nome inserito
+                })
+                .setNegativeButton(R.string.cancel, (dialog, id) -> {
+                    //non succede nulla
+                });
+        return builder.create();
+    }
+}
