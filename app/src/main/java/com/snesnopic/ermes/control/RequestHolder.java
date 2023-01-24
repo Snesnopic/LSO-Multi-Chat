e6183482d067947ce2e3dd28bf0967b56d9d2f21
Giuseppe <snesnopic@gmail.com>
2023-01-24 11:42:34 +0100

    Codice base per gestire le richieste singole

--- app/src/main/java/com/snesnopic/ermes/control/RequestHolder.java
+++ app/src/main/java/com/snesnopic/ermes/control/RequestHolder.java
@@ -2,26 +2,35 @@ package com.snesnopic.ermes.control;
 
 import android.content.Context;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.snesnopic.ermes.R;
 import com.snesnopic.ermes.datapkg.Request;
 
 public class RequestHolder extends RecyclerView.ViewHolder{
     private final TextView username;
     private final Context context;
+    private final ImageButton accept;
+    private final ImageButton refuse;
     private Request r;
     public RequestHolder(Context context, @NonNull View itemView) {
         super(itemView);
         this.context = context;
         username = itemView.findViewById(R.id.txt_requestUser);
+        accept = itemView.findViewById(R.id.acceptButton);
+        refuse = itemView.findViewById(R.id.refuseButton);
     }
     public void bindRequest(Request r)
     {
         this.r = r;
         username.setText(r.user.username);
+        accept.setOnClickListener(v -> {
+            //TODO: accetta richiesta di utente 'username' nel gruppo r.group.name
+        });
+        refuse.setOnClickListener(v -> {
+            //TODO: rifiuta richiesta di utente 'username' nel gruppo r.group.name
+        });
     }
 }
