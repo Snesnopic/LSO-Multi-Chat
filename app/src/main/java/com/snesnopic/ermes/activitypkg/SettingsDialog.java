e4ddd30a97cb46a2ce9631473c193c9365efae7c
Giuseppe <snesnopic@gmail.com>
2023-01-25 10:57:55 +0100

    Creato Dialog delle impostazioni

---
+++ app/src/main/java/com/snesnopic/ermes/activitypkg/SettingsDialog.java
@@ -0,0 +1,26 @@
+package com.snesnopic.ermes.activitypkg;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+import com.snesnopic.ermes.R;
+
+public class SettingsDialog extends DialogFragment {
+    @NonNull
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.settings_dialog, null));
+        builder.setMessage(R.string.settings)
+                .setPositiveButton(R.string.create, (dialog, id) -> {
+                    //TODO: applica modifiche, check del nome inserito e check della password
+                })
+                .setNegativeButton(R.string.cancel, (dialog, id) -> {
+                    //non succede nulla
+                });
+        return builder.create();
+    }
+}
