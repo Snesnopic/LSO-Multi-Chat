1a6b1b90f821c254cb498fb9227cb882afbd0e0d
Giuseppe <snesnopic@gmail.com>
2023-01-25 10:57:19 +0100

    Preset di codice per avere il valore immesso nella EditText

--- app/src/main/java/com/snesnopic/ermes/activitypkg/CreateGroupDialog.java
+++ app/src/main/java/com/snesnopic/ermes/activitypkg/CreateGroupDialog.java
@@ -4,6 +4,8 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.widget.EditText;
+
 import androidx.fragment.app.DialogFragment;
 import com.snesnopic.ermes.R;
 
@@ -15,6 +17,9 @@ public class CreateGroupDialog extends DialogFragment {
         builder.setMessage(R.string.insert_group_name)
                 .setPositiveButton(R.string.create, (dialog, id) -> {
                     //TODO: crea gruppo, check del nome inserito
+                    EditText et = getDialog().findViewById(R.id.editTextNewGroupName);
+                    String newGroupName = et.getText().toString();
+
                 })
                 .setNegativeButton(R.string.cancel, (dialog, id) -> {
                     //non succede nulla
