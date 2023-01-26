5207a907ac18464208d0bec6824c10c611a91ccb
Giuseppe <snesnopic@gmail.com>
2023-01-26 12:46:14 +0100

    Codice meno soggetto a bug

--- app/src/main/java/com/snesnopic/ermes/activitypkg/CreateGroupDialog.java
+++ app/src/main/java/com/snesnopic/ermes/activitypkg/CreateGroupDialog.java
@@ -1,28 +1,25 @@
 package com.snesnopic.ermes.activitypkg;
 
 import static com.snesnopic.ermes.activitypkg.LoginActivity.connection;
-
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageView;
-
 import androidx.fragment.app.DialogFragment;
-
-import com.google.android.material.snackbar.Snackbar;
 import com.snesnopic.ermes.R;
 
 public class CreateGroupDialog extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = requireActivity().getLayoutInflater();
-        builder.setView(inflater.inflate(R.layout.create_group_dialog, null));
+        View view = inflater.inflate(R.layout.create_group_dialog, null);
+        builder.setView(view);
         builder.setMessage(R.string.insert_group_name)
                 .setPositiveButton(R.string.create, (dialog, id) -> {
                     //TODO: crea gruppo, check del nome inserito
-                    EditText et = getDialog().findViewById(R.id.editTextNewGroupName);
+                    EditText et = view.findViewById(R.id.editTextNewGroupName);
                     String newGroupName = et.getText().toString();
                     if(connection.createGroup(newGroupName)) {
                         System.out.println("Gruppo creato");
