b41e5ab2b083f733c742eeccb3c929b3651b46ec
Giuseppe <snesnopic@gmail.com>
2023-01-27 10:41:24 +0100

    Cliccare su un gruppo ha un'azione diversa se ci fai parte o no

--- app/src/main/java/com/snesnopic/ermes/control/GroupHolder.java
+++ app/src/main/java/com/snesnopic/ermes/control/GroupHolder.java
@@ -1,6 +1,8 @@
 package com.snesnopic.ermes.control;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.View;
 import android.widget.TextView;
@@ -37,9 +39,21 @@ public class GroupHolder extends RecyclerView.ViewHolder implements View.OnClick
     @Override
     public void onClick(View v) {
         if (this.g != null) {
-            Intent mainIntent = new Intent(context, ChatActivity.class);
-            mainIntent.putExtra("groupName",g.name);
-            context.startActivity(mainIntent);
+            if (g.accessPermitted) {
+                Intent mainIntent = new Intent(context, ChatActivity.class);
+                mainIntent.putExtra("groupName", g.name);
+                context.startActivity(mainIntent);
+            }
+            else
+            {
+                new AlertDialog.Builder(v.getContext()).setMessage(R.string.do_you_want_to_join)
+                        .setNegativeButton(android.R.string.no, null)
+                        .setPositiveButton(android.R.string.yes, (dialog, which) -> {
+                            //TODO: aggiungere funzione che manda richiesta di unirsi a g
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+            }
         }
     }
 }
