a5bffed795b5618d8df3456e23addfcbaf0a8ff9
Giuseppe <snesnopic@gmail.com>
2023-01-28 10:26:40 +0100

    Collegato il pulsante per mandare la richiesta per entrare in un gruppo

--- app/src/main/java/com/snesnopic/ermes/control/GroupHolder.java
+++ app/src/main/java/com/snesnopic/ermes/control/GroupHolder.java
@@ -2,14 +2,14 @@ package com.snesnopic.ermes.control;
 
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.View;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-import com.snesnopic.ermes.activitypkg.ChatActivity;
 import com.snesnopic.ermes.R;
+import com.snesnopic.ermes.activitypkg.ChatActivity;
+import com.snesnopic.ermes.activitypkg.LoginActivity;
 import com.snesnopic.ermes.datapkg.Group;
 import com.snesnopic.ermes.datapkg.Message;
 import java.time.format.DateTimeFormatter;
@@ -49,7 +49,7 @@ public class GroupHolder extends RecyclerView.ViewHolder implements View.OnClick
                 new AlertDialog.Builder(v.getContext()).setMessage(R.string.do_you_want_to_join)
                         .setNegativeButton(android.R.string.no, null)
                         .setPositiveButton(android.R.string.yes, (dialog, which) -> {
-                            //TODO: aggiungere funzione che manda richiesta di unirsi a g
+                            LoginActivity.connection.makeJoinRequest(g);
                         })
                         .setIcon(android.R.drawable.ic_dialog_alert)
                         .show();
