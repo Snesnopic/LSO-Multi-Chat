3a9763774014065119589e083a139ccc85bd1970
giuseppe <snesnopic@gmail.com>
2023-02-15 13:20:52 +0100

    Accettando o rifiutando una richiesta essa verrà rimossa dalla lista

--- app/src/main/java/com/snesnopic/ermes/control/RequestHolder.java
+++ app/src/main/java/com/snesnopic/ermes/control/RequestHolder.java
@@ -2,13 +2,13 @@ package com.snesnopic.ermes.control;
 
 import static com.snesnopic.ermes.activitypkg.LoginActivity.connection;
 
-import android.content.Context;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.snesnopic.ermes.R;
+import com.snesnopic.ermes.activitypkg.RequestsFragment;
 import com.snesnopic.ermes.datapkg.Request;
 
 public class RequestHolder extends RecyclerView.ViewHolder{
@@ -25,7 +25,18 @@ public class RequestHolder extends RecyclerView.ViewHolder{
     public void bindRequest(Request r)
     {
         username.setText(r.user.username);
-        accept.setOnClickListener(v -> connection.requestHandler(r.group,r.user.userid,true));
-        refuse.setOnClickListener(v -> connection.requestHandler(r.group,r.user.userid,false));
+        System.out.println("Username: "+r.user.username + " Userid: "+r.user.userid);
+        accept.setOnClickListener(v ->
+        {
+            connection.requestHandler(r.group,r.user.userid,true);
+            Connessione.requests.removeIf(request -> request.group.id == r.group.id && request.user.userid == r.user.userid);
+            RequestsFragment.adapter.notifyDataSetChanged();
+        });
+        refuse.setOnClickListener(v ->
+        {
+            connection.requestHandler(r.group,r.user.userid,false);
+            Connessione.requests.removeIf(request -> request.group.id == r.group.id && request.user.userid == r.user.userid);
+            RequestsFragment.adapter.notifyDataSetChanged();
+        });
     }
 }
